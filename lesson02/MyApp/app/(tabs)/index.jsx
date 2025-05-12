2adacfda06bab279c6d52ea5f0a8b5bc097510d7
rob-schlenker <schlenk7@gmail.com>
2025-05-12 15:56:43 -0400

    Refactor tab layout and add contact screen; remove explore screen and update color constants

--- lesson02/MyApp/app/(tabs)/index.jsx
+++ lesson02/MyApp/app/(tabs)/index.jsx
@@ -1,5 +1,7 @@
-import { View, Text, StyleSheet, ImageBackground } from 'react-native'
+import { View, Text, StyleSheet, ImageBackground , Pressable} from 'react-native'
 import React from 'react'
+import { Link } from 'expo-router'
+
 
 import IcedCoffeeImg from '@/assets/images/iced-coffee.png'
 
@@ -11,7 +13,12 @@ const app = () => {
       resizeMode="cover"
       style={styles.image}      
       >
-        <Text style={styles.text}>Coffee Shop</Text>
+        <Text style={styles.title}>Coffee Shop</Text>
+        <Link href="/contact" style={{marginHorizontal: 'auto'}} asChild>
+        <Pressable style={styles.button}>
+          <Text style={styles.buttonText}>Contact Us</Text>  
+          </Pressable>
+        </Link>
       </ImageBackground>
     </View>
   )
@@ -31,11 +38,35 @@ const styles = StyleSheet.create({
     resizeMode: 'cover',
     justifyContent: 'center',
   },
-  text: {
+  title: {
     color: 'white',
     fontSize: 42,
     fontWeight: 'bold',
     textAlign: 'center',
-    backgroundColor: 'rgba(0, 0, 0, 0.5)',    git add .
+    backgroundColor: 'rgba(0, 0, 0, 0.5)', 
+    marginBottom: 120,
+  },
+  link: {
+    color: 'white',
+    fontSize: 42,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    textDecorationLine: 'underline',
+    padding: 4,
+  },
+  button: {
+    height: 60,
+    borderRadius: 20,
+    justifyContent: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.75)',
+    padding: 6,
+  },
+    buttonText: {
+    color: 'white',
+    fontSize: 16,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    padding: 4,
   },
 })
